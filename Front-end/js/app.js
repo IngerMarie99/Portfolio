87a8885f37587e982e42ca92402741dc0e5d96ab
Inger Marie Gunderhuset <ingermarie.gunderhuset@gmail.com>
2022-12-11 09:27:57 +0100

    removed second image at homepage

--- Front-end/js/app.js
+++ Front-end/js/app.js
@@ -18,7 +18,6 @@ const queryAllProjects = `
     project_name,
     slug,
     "main_image": main_image.asset->url,
-    "second_image":second_image.asset->url,
     }
     `;
 const querySingleProject = `
@@ -191,14 +190,11 @@ async function getAllProjects() {
         const mainImg = document.createElement('img');
         mainImg.setAttribute('src', project.main_image);
 
-        const secondImg = document.createElement('img');
-        secondImg.setAttribute('src', project.second_image);
 
         const titleElement = document.createElement('h2');
         titleElement.textContent = project.project_name;
 
         projectCard.append(mainImg);
-        projectCard.append(secondImg);
         projectCard.append(titleElement);
 
         
